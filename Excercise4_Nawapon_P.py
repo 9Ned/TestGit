98b433b9ce1beea7bc0654cdc99d774f371270d4
9Ned <nawapon19@gmail.com>
2020-10-19 15:52:33 +0700

    Create Excercise4_Nawapon_P.py

---
+++ Excercise4_Nawapon_P.py
@@ -0,0 +1,8 @@
+Foundation_English =  90.0
+General_Business =  85.0
+Introduction_to_Computer_Systems =  99.99
+Computer_Programming =  98.0
+print("Foundation_English:",Foundation_English)
+print("General_Business:",General_Business)
+print("Introduction_to_Computer_Systems:",Introduction_to_Computer_Systems)
+print("Computer_Programming:",Computer_Programming)
